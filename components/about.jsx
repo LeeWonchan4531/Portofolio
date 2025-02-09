29639d554589c6330a79a36afe060cc8d31b78a6
Lee Wonchan <wonchan@wonchan.net>
2025-02-09 13:02:47 +0900

    Update about.jsx

--- components/about.jsx
+++ components/about.jsx
@@ -8,6 +8,20 @@ const About = (props) => {
                     return <div className='mb-6'>{paragraph}</div>
                 })}
             </div>
+            <div className="flex flex-row">
+            <div className="bg-surface-400 py-1 px-3 rounded-md text-xs mr-2">HTML</div>
+            <div className="bg-surface-400 py-1 px-3 rounded-md text-xs mr-2">CSS</div>
+            <div className="bg-surface-400 py-1 px-3 rounded-md text-xs mr-2">JS</div>
+            <div className="bg-surface-400 py-1 px-3 rounded-md text-xs mr-2">PHP</div>
+            <div className="bg-surface-400 py-1 px-3 rounded-md text-xs mr-2">Python</div>
+            <div className="bg-surface-400 py-1 px-3 rounded-md text-xs mr-2">Fortinet</div>
+            </div>
+            <div className="flex flex-row mt-2">
+            <div className="bg-surface-400 py-1 px-3 rounded-md text-xs mr-2">React</div>
+            <div className="bg-surface-400 py-1 px-3 rounded-md text-xs mr-2">MariaDB</div>
+            <div className="bg-surface-400 py-1 px-3 rounded-md text-xs mr-2">Redis</div>
+            <div className="bg-surface-400 py-1 px-3 rounded-md text-xs mr-2">Linux Cluster</div>
+            </div>
         </div>
     )
 }
